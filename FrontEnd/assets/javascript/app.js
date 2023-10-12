af730ff7c98c99f6f93ce413341fb88d19484f85
Antoni Castagne <anto.casatgne@hotmail.fr>
2023-10-12 12:34:23 +0200

    modification pour ajout galerie depuis API

--- FrontEnd/assets/javascript/app.js
+++ FrontEnd/assets/javascript/app.js
@@ -1,4 +1,16 @@
-const gallery = document.querySelector(".gallery");
-console.log(gallery);
-gallery = fetch("http://localhost:5678/api/works").then(res => res.json());
-console.log(gallery);
+async function genererProjets (projets) {
+const reponse = await fetch("http://localhost:5678/api/works");
+const projet = await reponse.json();
+console.log(projet);
+    
+    for (let i = 0; i < projet.lenght;i ++){
+        const gallery = document.querySelector(".gallery");
+        const projets = projet[i];
+        const imageProjet = document.createElement("img");
+        const nomProjet = document.createElement("figcaption");
+
+        gallery.appendChild(projet);
+        gallery.appendChild(imageProjet);
+        gallery.appendChild(nomProjet);
+    };
+};
