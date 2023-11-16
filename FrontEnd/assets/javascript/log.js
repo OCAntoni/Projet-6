a3d425c8af4c4284f44018fbd78577d81df4fd04
Antoni Castagne <anto.casatgne@hotmail.fr>
2023-11-16 12:26:16 +0100

    resolution du pb de connexion avec identifiants aléatoire + message d'alerte

--- FrontEnd/assets/javascript/log.js
+++ FrontEnd/assets/javascript/log.js
@@ -1,5 +1,5 @@
 const email = document.getElementById("mail");
-const mdp = document.getElementById("mot-de-passe");
+const mdp = document.getElementById("mdp");
 const connexion = document.querySelector(".connect button");
 const errorEmail = document.querySelector(".mail-error");
 const errorMdp =document.querySelector(".mdp-error");
@@ -7,36 +7,20 @@ const errorMdp =document.querySelector(".mdp-error");
 //Fonction pour définir conditions d'accés 
 function verifierMail (email) {
     if (email.match(/[a-zA-z0-9._-]+@[a-z]+\.[a-z]+/)) {
-    console.log("Test OK");
     return true;
 
     } else {
-        console.log("Conditions non remplies");
         return false;
     };
 };
 
-function verifierMdp (mdp) {
-    if (mdp !== mdp.value) {
-        errorMdp.innerHTML = "Votre mot de passe n'est pas valide";
-        return false;
-    } else {
-        return true;
-    };
-};
 
 connexion.addEventListener("click",() => {
     if (!verifierMail(email.value)) {
         const errorMail = document.querySelector(".mail-error")
         errorEmail.innerHTML = "Veuillez saisir votre e-mail";
         console.log(errorMail);
-    }
-
-    if (verifierMdp()) {
-        
-    }
-
-    else {
+    } else {
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: {"content-type": "application/json; charset=UTF-8"},
@@ -47,8 +31,13 @@ connexion.addEventListener("click",() => {
         }) .then(res => res.json())
             .then(data => {
                 console.log(data)
+            
+                if (data.token) {
                 localStorage.setItem('token', data.token)
                 window.location.href = "/frontend/admin.html"
+                } else {
+                    alert("Votre email ou votre mot de passe est incorect !")
+                }
             })
     };
 });
